5c631ef62fe8344a19c4fc0ad2256137a31da93e
Berk <example@email.com>
2026-01-28 14:59:06 +0300

    feat: docker logic added frontend side

--- Logo/init.sql
+++ Logo/init.sql
@@ -3,24 +3,22 @@ CREATE SCHEMA IF NOT EXISTS logo;
 ALTER ROLE postgres SET search_path TO logo, public;
 
 CREATE TABLE IF NOT EXISTS customer (
-    id BIGSERIAL PRIMARY KEY ,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    balance DECIMAL(15,2) DEFAULT 0,
     address VARCHAR(255),
     country VARCHAR(100),
     local VARCHAR(100),
     district VARCHAR(100),
     vd_no VARCHAR(50),
     customer_code VARCHAR(255),
-    opening_balance DECIMAL(15,2);
-    archived BOOLEAN NOT NULL DEFAULT FALSE;
+    archived BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Örnek müşteriler
-INSERT INTO customer (name, balance, address, country, local, district, vdNo, customer_code, opening_balance)
+INSERT INTO customer (name, address, country, local, district, vd_no, customer_code)
 VALUES
-('Müşteri A', 1000.00, 'Adres 1', 'Türkiye', 'İstanbul', 'Kadıköy', '1234567890', 'dsadas', 0),
-('Müşteri B', 500.00, 'Adres 2', 'Türkiye', 'Ankara', 'Çankaya', '0987654321', 'dsadasdasd', 0);
+('Müşteri A', 'Adres 1', 'Türkiye', 'İstanbul', 'Kadıköy', '1234567890', 'K13'),
+('Müşteri B', 'Adres 2', 'Türkiye', 'Ankara', 'Çankaya', '0987654321', 'T51');
 
 -- ---------------------------
 -- 2. MALZEME TABLOSU
@@ -28,20 +26,24 @@ VALUES
 
 
 CREATE TYPE unit_status AS ENUM ('KG', 'ADET', 'M');
-
+CREATE TYPE currency_status AS ENUM ('TRY', 'EUR', 'USD');
 
 CREATE TABLE IF NOT EXISTS material (
     id BIGSERIAL PRIMARY KEY ,
     code VARCHAR(50) NOT NULL,
     comment VARCHAR(255),
-    unit unit_status NOT NULL
+    unit unit_status NOT NULL,
+    purchase_price DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    sales_price DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    purchase_currency currency_status NOT NULL DEFAULT 'TRY',
+    sales_currency currency_status NOT NULL DEFAULT 'TRY'
 );
 
 -- Örnek malzemeler
-INSERT INTO material (code, comment, unit)
+INSERT INTO material (code, comment, unit, purchase_price, sales_price, purchase_currency, sales_currency)
 VALUES
-('M001', 'Malzeme 1', 'ADET'),
-('M002', 'Malzeme 2', 'KG');
+('M001', 'Malzeme 1', 'ADET', 2.00, 5.00, 'EUR', 'USD'),
+('M002', 'Malzeme 2', 'KG', 3.00, 8.00, 'EUR', 'USD');
 
 -- ---------------------------
 -- 3. SATIN ALMA FATURASI TABLOSU
@@ -50,10 +52,12 @@ CREATE TABLE IF NOT EXISTS purchase_invoice (
     id BIGSERIAL PRIMARY KEY ,
     file_no VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
-    customer BIGINT NOT NULL,
+    customer_id BIGINT NOT NULL,
     total_price DECIMAL(15,2) DEFAULT 0,
     kdv_toplam DECIMAL(15,2) DEFAULT 0,
-    FOREIGN KEY (customer) REFERENCES customer(id)
+    eur_selling_rate DECIMAL(18, 4) DEFAULT 0.0000,
+    usd_selling_rate DECIMAL(18, 4) DEFAULT 0.0000,
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
 -- ---------------------------
@@ -77,13 +81,15 @@ CREATE TABLE IF NOT EXISTS purchase_invoice_item (
 -- ---------------------------
 CREATE TABLE IF NOT EXISTS sales_invoice (
     id BIGSERIAL PRIMARY KEY ,
-    fileNo VARCHAR(50) NOT NULL,
+    file_no VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
-    customer BIGINT NOT NULL,
+    customer_id BIGINT NOT NULL,
     total_price DECIMAL(15,2) DEFAULT 0,
     kdv_toplam DECIMAL(15,2) DEFAULT 0,
     type INTEGER,
-    FOREIGN KEY (customer) REFERENCES customer(id)
+    eur_selling_rate DECIMAL(15, 4) DEFAULT 0.0000,
+    usd_selling_rate DECIMAL(15, 4) DEFAULT 0.0000,
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
 -- ---------------------------
@@ -108,9 +114,15 @@ CREATE TABLE material_price_history (
     id BIGSERIAL PRIMARY KEY,
     material_id BIGINT,
     invoice_type invoice_status,
-    price DECIMAL(19,2),
+    invoice_id BIGINT,
+    price DECIMAL(18,2),
     date DATE,
     customer_name VARCHAR(255),
+    quantity DECIMAL(18, 2),
+    customer_id BIGINT,
+
+    CONSTRAINT cutomer_price
+        FOREIGN KEY (customer_id) REFERENCES customer(id),
     CONSTRAINT fk_material_price_history_material
         FOREIGN KEY (material_id) REFERENCES material(id)
 );
@@ -120,7 +132,7 @@ CREATE TABLE material_price_history (
 -- ---------------------------
 -- Örnek Satın Alma Faturası
 -- ---------------------------
-INSERT INTO purchase_invoice (fileNo, date, customer, total_price, kdv_toplam)
+INSERT INTO purchase_invoice (file_no, date, customer_id, total_price, kdv_toplam)
 VALUES ('PA-001', '2025-12-01', 1, 200, 36);
 
 INSERT INTO purchase_invoice_item (purchase_invoice_id, material_id, unit_price, quantity, line_total, kdv)
@@ -129,7 +141,7 @@ VALUES (1, 1, 100, 2, 200, 36);
 -- ---------------------------
 -- Örnek Satış Faturası
 -- ---------------------------
-INSERT INTO sales_invoice (fileNo, date, customer, total_price, kdv_toplam, type)
+INSERT INTO sales_invoice (file_no, date, customer_id, total_price, kdv_toplam, type)
 VALUES ('SA-001', '2025-12-01', 2, 300, 54, 1);
 
 INSERT INTO sales_invoice_item (sales_invoice_id, material_id, unit_price, quantity, line_total, kdv)
@@ -145,6 +157,7 @@ CREATE TABLE IF NOT EXISTS received_collection (
     price DECIMAL(15,2) NOT NULL,
     customer_id BIGINT,
     customer_name VARCHAR(255),
+    file_no VARCHAR(255),
     CONSTRAINT fk_received_customer FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
@@ -158,6 +171,7 @@ CREATE TABLE IF NOT EXISTS payment_company (
     price DECIMAL(15,2) NOT NULL,
     customer_id BIGINT,
     customer_name VARCHAR(255),
+    file_no VARCHAR(255),
     CONSTRAINT fk_payment_customer FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
@@ -166,7 +180,7 @@ CREATE TABLE IF NOT EXISTS app_user(
 id BIGSERIAL PRIMARY KEY,
 username VARCHAR(255) NOT NULL,
 password VARCHAR(255) NOT NULL
-)
+);
 
 -- Şirket Tablosu
 CREATE TABLE IF NOT EXISTS company (
@@ -174,7 +188,7 @@ id BIGSERIAL PRIMARY KEY,
 name VARCHAR(255),
 schema_name VARCHAR(255),
 description VARCHAR(255)
-)
+);
 
 CREATE TYPE payroll_model_enum AS ENUM ('INPUT', 'OUTPUT');
 CREATE TYPE payroll_type_enum AS ENUM ('CHEQUE', 'BOND');
@@ -193,6 +207,32 @@ CREATE TABLE IF NOT EXISTS payroll (
     amount DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
 
     CONSTRAINT fk_payroll_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES customer(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS currency_rate (
+    id SERIAL PRIMARY KEY,
+    currency VARCHAR(255),
+    buying_rate DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    selling_rate DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    last_updated DATE
+);
+
+CREATE TABLE IF NOT EXISTS opening_voucher (
+    id SERIAL PRIMARY KEY,
+    customer_id BIGINT,
+    customer_name VARCHAR(255),
+    file_no VARCHAR(255),
+    description VARCHAR(255),
+    date DATE,
+    debit DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    credit DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    final_balance DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    yearly_credit DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+    yearly_debit DECIMAL(18, 2) NOT NULL DEFAULT 0.00,
+
+        CONSTRAINT voucher_customer
         FOREIGN KEY (customer_id)
         REFERENCES customer(id) ON DELETE CASCADE
 );
